14ebe52cd85d9beb5cf4eef5a778b10b86339cf1
Matthew Gregg <matthew.gregg@livingsocial.com>
2015-09-30 16:23:44 -0400

    Don't need these anymore, just for testing/dev

--- bin/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-weave launch
-docker run -d  --name web01 -v /vagrant/docker/nginx/sites-enabled:/etc/nginx/sites-enabled -v /vagrant/docker/nginx/html:/var/www/html homework/nginx
-docker run -d -p 80:80 -p 9000:9000 --name lb01 -v /vagrant/docker/haproxy/config:/haproxy-override -ti homework/haproxy
